0b69574c8b83990f3a4a53e15ed55f9dbfc6c77c
mcanalyticai <tech@mc-analyticai.com>
2023-03-11 12:38:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>
+              Name:
+              <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
